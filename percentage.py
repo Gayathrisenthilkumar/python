a93195af255b6b390751e1952e6d3a4d762a772b
Gayathrisenthilkumar <164990837+Gayathrisenthilkumar@users.noreply.github.com>
2025-01-30 11:17:58 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,17 @@
+score=int(input("score percentage"))
+if(score>=70):
+    name=input("enter your name")
+    age=int(input("enter your age"))
+    location=input("enter your department")
+    print("you are eligible")
+else:
+    print("not eligible")
+
+
+
+
+
+
+
+
+
